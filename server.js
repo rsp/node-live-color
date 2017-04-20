9f323f43421b6f6346205a7cc32618e583099cb7
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-04-21 01:31:17 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.Server(app);
+const io = socket(server);
+
+const port = process.env.PORT || 3338;
+
+let color = '#ffffff';
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', express.static(path.join(__dirname, 'html')));
+
+io.on('connection', (s) => {
+  console.log('Socket.io client connected');
+  s.emit('color', color);
+});
+
+app.post('/color', (req, res) => {
+  color = req.body.color;
+  console.log('Changing color to', color); 
+  io.emit('color', color);
+  res.send({color: color});
+});
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
+
+console.log('node-live-color example - see: https://github.com/rsp/node-live-color');
